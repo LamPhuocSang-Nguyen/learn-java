187b21c7535f99f9ab33d8157b8e7a730f53c87e
unknown <LPSang@DESKTOP-8FL2I08>
2021-07-12 17:14:51 +0700

    test

---
+++ project1/src/ComputeAreaWithConsoleInput.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LPSang
+ */
+
+import java.util.Scanner;
+
+public class ComputeAreaWithConsoleInput {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter a number for radius: ");
+        double radius = input.nextDouble();
+        
+        double area = radius * radius * 3.14159;
+        
+        System.out.println("The area for the circle of radius " + 
+                radius + " is " + area);
+    }
+    
+}
