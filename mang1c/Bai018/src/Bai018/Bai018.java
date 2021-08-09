c83c50e05a569af0b08bd882cb1d2b687f872085
unknown <LPSang@DESKTOP-8FL2I08>
2021-08-09 22:22:18 +0700

    Toi bai 020 mang1c

---
+++ mang1c/Bai018/src/Bai018/Bai018.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bai018;
+
+/**
+ *
+ * @author LPSang
+ */
+import java.util.Scanner;
+
+public class Bai018 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter the number of element: ");
+        
+        int n = input.nextInt();
+        
+        int[] a = new int[n];
+        
+        InputElement(a);
+        
+        OutputElement(a);
+        
+        Enumerate(a);
+    }
+    
+    public static void InputElement(int[] a)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        for(int i = 0; i < a.length; i++)
+        {
+            //(Math.random() * (max - min)) + min 
+            a[i] = (int)(Math.random() * (100 + 100)) - 100;
+        }
+    }
+    
+    public static void OutputElement(int[] a)
+    {
+        for(int i = 0; i < a.length; i++)
+        {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+    
+    public static void Enumerate(int[] a)
+    {
+        for(int i = 0; i < a.length; i++)
+        {
+            if(a[i] == MaxNumber(a))
+            {
+                System.out.println(i);
+            }
+        }
+        
+    }
+    
+    public static int MaxNumber(int[] a)
+    {
+        int max = 0;
+        
+        for(int i = 0; i < a.length; i++)
+        {
+            if(a[i] > max)
+            {
+                max = a[i];
+            }    
+        }
+        return max;
+    }
+}
