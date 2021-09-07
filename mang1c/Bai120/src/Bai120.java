69a264a3801a8b3a42f22e686d67eff44d528b70
unknown <LPSang@DESKTOP-8FL2I08>
2021-09-07 16:20:26 +0700

    Toi bai120 mang1c

---
+++ mang1c/Bai120/src/Bai120.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LPSang
+ */
+import java.util.Scanner;
+
+public class Bai120
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter the number of element: ");
+        
+        int n = input.nextInt();
+        
+        int[] a = new int[n];
+        
+        InputElement(a);
+        
+        OutputElement(a);
+        
+        int kq = KtTonTai(a);
+        if(kq != 0)
+        {
+            System.out.println("mang co ton tai so nguyen to");
+        }
+        else
+        {
+            System.out.println("mang khong ton tai so nguyen to");
+        }
+    }
+    public static void InputElement(int[] a)
+    {
+        for(int i = 0; i < a.length; i++)
+        {
+            a[i] = (int)(Math.random() * (100 + 100) - 100);
+        }
+    }
+    public static void OutputElement(int[] a)
+    {
+        for(int i = 0; i < a.length; i++)
+        {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+    public static boolean KtNguyenTo(int a)
+    {
+        int Dem = 0;
+        
+        for(int i = 1; i <= a; i++)
+        {
+            if(a % i == 0)
+            {
+                Dem++;
+            }
+        }
+        if(Dem == 2)
+        {
+            return true;
+        }
+        return false;
+    }
+    public static int KtTonTai(int[] a)
+    {
+        for(int i = 0; i < a.length; i++)
+        {
+            if(KtNguyenTo(a[i]))
+            {
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
